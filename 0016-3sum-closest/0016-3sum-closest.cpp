5a31366a3996fb9c3a236c6d4b19c46fba0946d9
sanidhya17N <84037096+sanidhya17N@users.noreply.github.com>
2023-05-11 20:09:17 +0530

    Update 0016-3sum-closest.cpp

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int twoSumClosest(vector<int>&nums,int target,int i){
+       /* this function will traverse the array in two pointer manner , if target - (nums[i] + nums[j] )< minGap , we will store the ans . 
+          if our sum < target i++ else j-- . Logic being array is sorted so it to get closer it is understood that i++ will be required at lesser sum and 
+          j-- at the reverse condition
+       */
+        
         int ans=0;
         int j=nums.size()-1;
         int minGap=INT_MAX;
@@ -19,14 +24,16 @@ public:
         
     }
     int threeSumClosest(vector<int>& nums, int target) {
-         int ans=INT_MAX;
+         int minGap=INT_MAX;
          int sum=0;
          int n=nums.size();
+         // Sorting will help in twoSumClosest Function
          sort(nums.begin(),nums.end());
          for(int i=0;i<n-2;i++){
+             // twoSumClosest returns two numbers with closet sum to (target - nums[i])  
              int temp=twoSumClosest(nums,target-nums[i],i+1);
-             if(abs(target-(temp+nums[i]))<ans){
-                 ans=abs(target-(temp+nums[i]));
+             if(abs(target-(temp+nums[i]))<minGap){
+                 minGap=abs(target-(temp+nums[i])); //updating minGap is a closer ans is achieved
                  sum=temp+nums[i];
              }
          }
