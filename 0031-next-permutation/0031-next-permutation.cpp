1e9df88a28b354ac8cfcd14d2d402b2cdd932372
sanidhya17N <84037096+sanidhya17N@users.noreply.github.com>
2023-06-22 09:14:04 +0530

    Time: 8 ms (30.9%), Space: 12 MB (93.64%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,8 +1,35 @@
 class Solution {
 public:
+    void reverse(vector<int>& nums,int start){
+        int n=nums.size();
+        for(int i=start ; i<=(n+start)/2-1 ; i++){
+            swap(nums[i],nums[n-(i-start)-1]);
+        }
+    }
     void nextPermutation(vector<int>& nums) { 
         int n=nums.size();
-        next_permutation(nums.begin(),nums.end());
+        int breakIndex=-1;
+        for(int i=n-2 ;i>=0 ;i--){
+            if(nums[i]<nums[i+1]){
+                breakIndex=i;
+                break;
+            }
+        }
+
+        if(breakIndex!=-1){
+            
+            for(int i=n-1;i>=0;i--){
+                if(nums[i]>nums[breakIndex]){
+                    swap(nums[i],nums[breakIndex]);
+                    break;
+                }
+            }
+            reverse(nums,breakIndex+1);
+        }else{
+            reverse(nums,0);
+        }
+
+
 
         
     }
